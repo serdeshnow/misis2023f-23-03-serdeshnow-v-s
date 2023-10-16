ebc4e29fe90157cc949a995868bae5be45e17dcf
vlad <69970027+serdeshnow@users.noreply.github.com>
2023-10-16 11:37:42 +0300

    Create 1585A.cpp

---
+++ prj.codeforces/1585A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+//#include <vector>
+
+int main() {
+	int t = 0;
+	std::cin >> t;
+	//std::vector <int> result;
+	while (t != 0) {
+		int hight = 1;
+		int n = 0;
+		std::cin >> n;
+		int a = -1, a0 = -2;
+		while (n != 0) {
+			std::cin >> a;
+			if (a == 0 && a0 == a) {
+				hight = -1000;
+			}
+			else if (a == 1 && a0 != 1){
+				hight++;
+			}
+			else if (a == 1 && a0 == a) {
+				hight += 5;
+			}
+			a0 = a;
+			n--;
+		}
+		if (hight < 0) {
+			std::cout << -1 << std::endl;
+		}
+		else {
+			std::cout << hight << std::endl;
+		}
+		t--;
+	}
+	return 0;
+}
