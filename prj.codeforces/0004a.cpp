1fb5d33f24e93f5b8aeb22613fa5207013ac52a7
Serdeshnow <69970027+serdeshnow@users.noreply.github.com>
2023-10-30 13:54:37 +0300

    Update 0004a.cpp

--- prj.codeforces/0004a.cpp
+++ prj.codeforces/0004a.cpp
@@ -1,2 +1,13 @@
-----
-  
+#include <iostream>
+#include <string>
+ 
+int main()
+{
+  int w = 0;
+  std::cin >> w;
+  if (0 == w%2 && 2<w) {
+      std::cout<<"YES";
+  }else{
+      std::cout<<"NO";
+  }
+}
