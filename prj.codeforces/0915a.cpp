f1989f67398d258668bf5f483b9e3382d34ef001
Serdeshnow <69970027+serdeshnow@users.noreply.github.com>
2023-11-12 14:21:41 +0300

    Create 0915a.cpp

---
+++ prj.codeforces/0915a.cpp
@@ -0,0 +1,17 @@
+//915A
+#include <iostream>
+ 
+int main() {
+	int n = 0, k = 0;
+	std::cin >> n >> k;
+	int min_hours = 100000;
+	while (n--) {
+		int a = 0;
+		std::cin >> a;
+		if ((k % a == 0) && ((k / a) < min_hours)) {
+			min_hours = (k / a);
+		}
+	}
+	std::cout << min_hours;
+	return 0;
+}
