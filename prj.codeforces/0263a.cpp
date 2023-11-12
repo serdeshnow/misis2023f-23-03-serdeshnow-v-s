81f6deab160c07cde87b2c3281f40dc3c6ae1714
Serdeshnow <69970027+serdeshnow@users.noreply.github.com>
2023-11-12 14:08:00 +0300

    Create 0263a.cpp

---
+++ prj.codeforces/0263a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+ 
+int main() {
+	// центр: i, j: (2, 2)
+	int x = 0, y = 0;
+	for (int i = 0; i < 5; i++) {
+		//std::string str;
+		//std::cin >> str;
+		for (int j = 0; j < 5; j++) {
+			int t = 0;
+			std::cin >> t;
+			if (t == 1) {
+				x = i, y = j;
+			}
+		}
+	}
+	int count = std::fabs(x - 2) + std::fabs(y - 2);
+	std::cout << count;
+	return 0;
+}
