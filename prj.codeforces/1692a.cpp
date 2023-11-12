ea09c88fd3f95d9a6ad06cd556671192e8419368
Serdeshnow <m2303725@edu.misis.ru>
2023-11-12 23:04:02 +0300

    Create 1692a.cpp & Update CMakeLists.txt

---
+++ prj.codeforces/1692a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+	int t = 0;
+	std::cin >> t;
+	while (t--) {
+		int a = 0; // Timur
+		int b = 0;
+		int c = 0;
+		int d = 0;
+		std::cin >> a >> b >> c >> d;
+		int count = 0;
+		if (a > b) {
+			count++;
+		}
+		if (a > c) {
+			count++;
+		}
+		if (a > c) {
+			count++;
+		}
+		std::cout << count;
+	}
+	return 0;
+}
