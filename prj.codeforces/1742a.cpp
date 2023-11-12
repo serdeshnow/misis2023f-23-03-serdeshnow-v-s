2466099db4d6c134ada2e0fcaac89775bb3e4c54
Serdeshnow <69970027+serdeshnow@users.noreply.github.com>
2023-11-12 19:12:41 +0300

    Create 1742a.cpp

---
+++ prj.codeforces/1742a.cpp
@@ -0,0 +1,19 @@
+//
+#include <iostream>
+
+int main() {
+	int t = 0;
+	std::cin >> t;
+	while (t--) {
+		int a, b, c;
+		std::cin >> a >> b >> c;
+		if ((a + b == c) || (a + c == b) || (b + c == a)) {
+			std::cout << "YES" << std::endl;
+		}
+		else {
+			std::cout << "NO" << std::endl;
+		}
+	}
+
+	return 0;
+}
