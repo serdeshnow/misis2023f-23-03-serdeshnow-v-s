9173de3248f4f7ce2b70e277896f732b460d11fb
Serdeshnow <69970027+serdeshnow@users.noreply.github.com>
2023-11-12 19:29:42 +0300

    Create 1742b.cpp

---
+++ prj.codeforces/1742b.cpp
@@ -0,0 +1,31 @@
+//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+ 
+int main() {
+	int t = 0;
+	std::cin >> t;
+	while (t--) {
+		int n = 0;
+		std::cin >> n;
+		std::vector <int> arr;
+		for (int i = 0; i < n; i++) {
+			int a = 0;
+			std::cin >> a;
+			arr.push_back(a);
+		}
+		std::sort(arr.begin(), arr.end());
+ 
+		bool flag = true;
+		for (int j = 0; j < n - 1; j++) {
+			if (arr[j] == arr[j + 1]) {
+				flag = false;
+				break;
+			}
+		}
+		std::cout << ((flag) ? "YES\n" : "NO\n");
+	}
+ 
+	return 0;
+}
