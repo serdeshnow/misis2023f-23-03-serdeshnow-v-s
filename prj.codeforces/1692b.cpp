b716ed3e1281b9a0e4a976f76259823e3b969347
Serdeshnow <m2303725@edu.misis.ru>
2023-11-13 14:54:25 +0300

    Create 1692b Rename 1692b_v1 Edited global CMakeLists

--- prj.codeforces/1692b.cpp
+++ prj.codeforces/1692b.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 
-
 int main() {
 	int t = 0;
 	std::cin >> t;
@@ -15,21 +14,11 @@ int main() {
 			arr.push_back(a);
 		}
 
-		for (int i = 0; i < arr.size(); i++) {
-			for (int j = i + 1; j < arr.size(); j++) {
-				if (arr[i] == arr[j] && arr[i] != -1) {
-					arr[j] = -1;
-				}
-			}
-		}
 
-		int count = 0;
-		for (int k = 0; k < arr.size(); k++) {
-			if (arr[k] != -1) {
-				count += 1;
-			}
-		}
-		std::cout << count << std::endl;
+
+
+
+
 	}
-	return 0;
+  return 0;
 }
