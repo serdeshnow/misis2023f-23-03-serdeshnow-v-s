a8a5ecef2708cc367d517c96faa9e2bfa3a78103
Serdeshnow <m2303725@edu.misis.ru>
2023-11-13 16:22:25 +0300

    Update 1559a.cpp

---
+++ prj.codeforces/1559a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  int t = 0;
+  std::cin >> t;
+  while (t--) {
+    int n = 0;
+    std::vector <int> arr;
+    for (int index = 0; index < n; index++) {
+      int a = 0;
+      std::cin >> a;
+      arr.push_back(a);
+    }
+    
+    int l = 0;
+    int r = 0;
+    std::cin >> l >> r;
+    l--;
+    r--;
+    std::vector <int> arr_slice(l, r);
+
+    for (int i = 0; i < (arr_slice.size() + 1) / 2; i++) {
+      arr_slice[i] = arr_slice[i] & arr_slice[arr_slice.size() - i];
+    }
+    
+    int maxi = 0;
+    for (int j = 0; j < arr_slice.size(); j++) {
+      maxi = std::max(arr_slice[j], maxi);
+    }
+    std::cout << maxi;
+  }
+  return 0;
+}
