35093c6d2b4c743c93bbb5be0996195263107849
Serdeshnow <m2303725@edu.misis.ru>
2023-12-06 15:31:23 +0300

    Update rational.cpp

--- prj.lab/rational/rational.cpp
+++ prj.lab/rational/rational.cpp
@@ -31,8 +31,8 @@ public:
   bool operator<(const int rhs) const { return operator<=(rhs) && !operator==(rhs); }
 
   // Операторы арифметические:
-  Rational& operator+=(const Rational& rhs);
-  Rational& operator+=(const int rhs) { return operator+=(Rational(rhs)); }
+  Rational& operator+=(const Rational& rhs); // объявление оператора
+  Rational& operator+=(const int rhs) { return operator+=(Rational(rhs)); } // объявление и определение
 
   Rational& operator-=(const Rational& rhs);
   Rational& operator-=(const int rhs) { return operator-=(Rational(rhs)); }
@@ -49,11 +49,56 @@ private:
   int num_ = 0; // числитель 
   int din_ = 1; // знаменатель
 
+  int gcd(int a, int b) { // НОД, китайская теорема об остатках
+    if (a * b == 0) {
+      return a + b; // сам НОД
+    }
+    else if (a > b) {
+      return gcd(a % b, b);
+    }
+    else {
+      return gcd(b % a, a);
+    }
+  }
+
+  void fix() {
+    bool is_positive = (num_ * din_ >= 0 ? true : false); // знак дроби
+    num_ = std::abs(num_);
+    din_ = std::abs(din_);
+    int nod = gcd(num_, din_);
+    num_ /= nod;
+    din_ /= nod;
+    num_ = (is_positive ? num_ : -num_);
+  }
+
 };
 
 Rational& Rational::operator+=(const Rational& rhs) {
   num_ = num_ * rhs.din_ + din_ * rhs.num_;
   din_ = din_ * rhs.din_;
+  fix();
   return *this;
 }
+
+Rational& Rational::operator-=(const Rational& rhs) {
+  num_ = num_ * rhs.din_ - din_ * rhs.num_;
+  din_ = din_ * rhs.din_;
+  fix();
+  return *this;
+}
+
+Rational& Rational::operator*=(const Rational& rhs) {
+  num_ = num_ * rhs.num_;
+  din_ = din_ * rhs.din_;
+  fix();
+  return *this;
+}
+
+Rational& Rational::operator/=(const Rational& rhs) {
+  num_ = num_ * rhs.din_;
+  din_ = din_ * rhs.num_;
+  fix();
+  return *this;
+}
+
 
